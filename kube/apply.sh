6235ada4377fa4e9163f525e94e0e85e92963c9f
Q <q@misell.cymru>
2020-04-18 15:20:38 +0100

    add auth to apps

---
+++ kube/apply.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+VERSION=$(sentry-cli releases propose-version || exit)
+
+sed -e "s/(version)/$VERSION/g" < django.yaml | kubectl apply -f - || exit
+#kubectl apply -f nginx.yaml || exit
+
+sentry-cli releases --org we-will-fix-your-pc deploys $VERSION new -e prod
