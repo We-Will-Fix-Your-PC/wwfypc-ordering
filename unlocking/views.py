d053eb1c377ea898edbb78550bb183fa11fa064a
Q <q@misell.cymru>
2020-05-03 12:14:29 +0100

    x-frame-options

--- unlocking/views.py
+++ unlocking/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
+@xframe_options_exempt
 def order_widget(request):
     return render(request, "unlocking/order_widget.html", {
         "accepts": request.META.get("HTTP_ACCEPT")
